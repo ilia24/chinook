6e47879afd821f185ff727f89050a888879653f0
ilia24 <iliademertchian24@gmail.com>
2017-04-13 10:46:46 -0400

    modified schema

--- chinook_app/app/models/track.rb
+++ chinook_app/app/models/track.rb
@@ -1,2 +1,17 @@
 class Track < ActiveRecord::Base
+  SHORT = 180000
+  LONG = 360000
+
+  scope :short, -> {where('milliseconds < ?', SHORT)}
+  scope :medium, -> {where('milliseconds >= ? AND milliseconds < ?', SHORT, LONG)}
+  scope :long, -> {where('milliseconds >?', LONG)}
+
+  scope :starts_with, -> (first_char){where('name ILIKE ?', "#{first_char}%")}
+
+  scope :starts_with_a, -> {where('name ILIKE ?', 'a%')}
+
+  # def self.short
+  #   where('milliseconds < ?', SHORT)
+  # end
+
 end
--- chinook_app/db/schema.rb
+++ chinook_app/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128205156) do
+ActiveRecord::Schema.define(version: 20170413144302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140128205156) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "ranking"
   end
 
   create_table "artists", force: :cascade do |t|
